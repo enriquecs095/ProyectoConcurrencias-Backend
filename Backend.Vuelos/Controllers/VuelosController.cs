1f8223c56ed62ea3da567b47284330b43474b936
Siumauricio <Siumauricio@unitec.edu>
2021-03-06 17:19:21 -0600

    Creacion de APIS y Conexion a MongoDB

---
+++ Backend.Vuelos/Controllers/VuelosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend_Vuelos.Models;
+using Backend_Vuelos.Services;
+
+namespace Backend_Vuelos.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class VuelosController : Controller
+    {
+        private readonly ConcurrenciaServices _vuelos;
+
+        public VuelosController(ConcurrenciaServices vuelos)
+        {
+            _vuelos = vuelos;
+        }
+
+        [HttpGet("getAll")]//obtiene todos los vuelos disponibles
+        public ActionResult<List<Vuelos>> getVuelos() =>
+        _vuelos.getVuelos();
+
+        [HttpPost("getByOrigin")]
+        public ActionResult<List<Vuelos>> getByOrigin([FromBody] Origenes_Destino origin) {
+            return _vuelos.getVuelosByOrigin(origin);
+
+        }
+        [HttpPost("getByDestination")]
+        public ActionResult<List<Vuelos>> getByDestination([FromBody] Origenes_Destino origin) {
+            return _vuelos.getVuelosByDestination(origin);
+        }
+        [HttpGet("getByDate/date")]
+        public ActionResult<List<Vuelos>> getByDate(string date) =>
+            _vuelos.getVuelosByDate(date);
+
+        [HttpPost("getByRange/{minimumRange}/{maximumRange}")]
+        public ActionResult<List<Vuelos>> getByRange(double minimumRange, double maximumRange) =>
+                _vuelos.getVuelosByRange(minimumRange, maximumRange);
+    }
+}
---
+++ Backend.Vuelos/Models/Vuelos.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Vuelos.Models
+{
+    public class Vuelos
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public int Disponibilidad { get; set; }
+        public Origenes_Destino Lugar_Origen { get; set; }
+        public Origenes_Destino Lugar_Destino { get; set; }
+        public int Clase { get; set; }
+        public double Precio { get; set; }  
+        public string Aerolinea { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+
+    }
+}
